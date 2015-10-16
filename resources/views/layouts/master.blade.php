327b4a9b0f1dc378f519b7f2b7e2b813d4207917
Priyanka <priyanka.devzila.com>
2015-10-17 00:55:45 +0530

    Change page title

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Humbole</title>
+    <title>Humbole | Your Daily Dose...</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Your daily dose">
     <meta name="author" content="">
